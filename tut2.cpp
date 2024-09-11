5afa0f70056e7132da1da9644eb5b475f144a4c4
Avinash96-gthb <113711198+Avinash96-gthb@users.noreply.github.com>
2024-09-11 22:49:01 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"The first number is: "<<endl;
+    cin>>a;
+    cout<<"The second number is: "<<endl;
+    cin>>b;
+    cout<<"The sum is: "<<a+b<<endl;
+    return 0;
+}
